f891d45e4cc4320650852fd31e8114fbca24e5fe
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2019-08-12 22:15:53 +0200

    Modifying board

--- editor/src/App.js
+++ editor/src/App.js
@@ -1,26 +1,95 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    let monsters = [];
+    for (let i = 0; i < 20; i++) {
+      monsters.push(i);
+    }
+
+    let fields = [];
+    for (let i = 0; i < 49; i++) {
+      fields.push({p: i, t: null});
+    }
+
+    this.state = {
+      monsters,
+      fields,
+      current: 0
+    };
+
+    this.setMonster = this.setMonster.bind(this);
+    this.setField = this.setField.bind(this);
+  }
+
+  setMonster(val) {
+    this.setState({
+      current: val
+    })
+  }
+
+  setField(p) {
+    this.setState(prevState => {
+      const _fields = [...prevState.fields];
+      _fields[p] = {p, t: this.state.current};
+        return {
+          fields: _fields
+        }
+    });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="app">
+        <p>Pick the monster to place</p>
+        <div>
+          <input
+            key={'zero'}
+            type={"button"}
+            value={'CLEAR'}
+            disabled={this.state.current === null}
+            onClick={() => this.setMonster(null)}
+          />
+          {this.state.monsters.map(val => {
+            return <input
+              key={val}
+              type={"button"}
+              value={val}
+              className={(val.p + 1) % 5 === 0 ? 'boss' : ''}
+              disabled={this.state.current === val}
+              onClick={() => this.setMonster(val)}
+              />;
+          })}
+        </div>
+
+        <div>
+          {this.state.fields.map(val => {
+            if (val.p % 7 === 0) {
+              return <div className="btn" key={val.p}>
+                <br/>
+                <input
+                  type={"button"}
+                  value={val.t !== null ? val.t : ''}
+                  onClick={() => this.setField(val.p)}
+                />
+              </div>;
+            }
+            return <div className="btn" key={val.p}>
+              <input
+                type={"button"}
+                value={val.t !== null ? val.t : ''}
+                onClick={() => this.setField(val.p)}
+              />
+            </div>;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
